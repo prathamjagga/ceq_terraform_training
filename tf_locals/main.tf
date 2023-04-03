96a1eb9c96d677d069b59de567f0f6f55a3a3fcf
unknown <prathamjagga123@gmail.com>
2023-04-03 14:48:52 +0530

    day2 local practice

--- tf_locals/main.tf
+++ tf_locals/main.tf
@@ -5,7 +5,8 @@
 
 
 locals {
-  content = "<h3>hello world i am pratham</h3>"
+  content      = "<h3>hello world i am pratham</h3>"
+  content_list = ["hello", "pratham"]
 }
 
 resource "local_file" "file-block" {
@@ -13,3 +14,9 @@ resource "local_file" "file-block" {
   filename = "hello.html"  // necessary
 }
 
+
+
+resource "local_file" "res2" {
+  content  = local.content_list[0] // optional 
+  filename = "hello2.html"         // necessary
+}
